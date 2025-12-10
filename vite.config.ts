0c3f574e68987bf50d46d66880ad0a834f12bdfa
Caio Guilherme Ferreira <78062972+caioconstantino@users.noreply.github.com>
2025-12-10 04:31:59 -0300

    tentativa de deploy

--- vite.config.ts
+++ vite.config.ts
@@ -6,16 +6,17 @@ import { componentTagger } from "lovable-tagger";
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
   server: {
-    host: "0.0.0.0",
+    host: true,
     port: 8080,
     allowedHosts: ['trusthbpo.com'],
-  },  
+  },
   preview: {
-    host: "0.0.0.0",
-    port: Number(process.env.PORT) || 4173,
+    host: true,
+    // a porta aqui tanto faz, porque no servidor você está passando `--port $PORT`,
+    // mas deixo uma default:
+    port: 4173,
     allowedHosts: ["trusthbpo.com"],
   },
-  
   plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
   resolve: {
     alias: {
